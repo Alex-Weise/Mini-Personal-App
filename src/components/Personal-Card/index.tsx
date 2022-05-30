9c97ceda938bbfaec9b36ee56c9c2c9b2c485489
Alex-Weise <Sannek65@gmail.com>
2022-05-30 20:41:06 +0300

    fix5

--- src/components/Personal-Card/index.tsx
+++ src/components/Personal-Card/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, FC } from "react";
 import styles from "./styles.module.scss"
 import Card from "./Card"
 import { TpropsContent } from "./type.module";
-
+import CircularProgress from '@mui/material/CircularProgress';
 // async function Content () {
 //     let response = await fetch('https://dummyjson.com/products/category/motorcycle');
 //     let content = await response.json();
@@ -15,20 +15,23 @@ import { TpropsContent } from "./type.module";
 
 const PersonalCard:FC = () => {
   const [content, setContent] = useState<TpropsContent[]>([])
+  const [isLoade, setIsLoade] = useState(true);
 
     useEffect( () => {
        fetch('https://dummyjson.com/products?limit=10&skip=20')
                 .then(response => response.json())
-                .then(obj => setContent(obj.products));
+                .then(obj => setContent(obj.products))
+                .finally( () => setIsLoade(false));
        return setContent([]);
     }, [])
 
     return (
         <section className={styles.section_card}>
-            {content.map( (item) => {
+           { isLoade ? (<CircularProgress color="inherit"/>): 
+           (content.map( (item) => {
                 return (<Card title={item.brand} img={item.images[0]} 
                     discr={item.description} key={item.id}/>)
-            })}
+            }))}
         </section>
     );
 }
