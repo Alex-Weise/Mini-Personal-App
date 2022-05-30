22866391dc62a6c6e466d3a7372b632baa02d5d8
Alex-Weise <Sannek65@gmail.com>
2022-05-30 19:23:47 +0300

    fix4

---
+++ src/components/Personal-Card/type.module.ts
@@ -0,0 +1,11 @@
+import { type } from "os";
+import React from "react";
+
+type TpropsContent = {
+    brand: string;
+    images: string[];
+    description: string;
+    id: string;
+}
+
+export type {TpropsContent};
