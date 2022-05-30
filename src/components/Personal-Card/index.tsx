6adacfd6e73885918020629f09126be148593f31
Alex-Weise <Sannek65@gmail.com>
2022-05-30 15:27:44 +0300

    fix3

--- src/components/Personal-Card/index.tsx
+++ src/components/Personal-Card/index.tsx
@@ -2,22 +2,16 @@ import React from "react";
 import styles from "./styles.module.scss"
 import Card from "./Card"
 
-
-const CONTENT = [
-    {title: 'BERGMUND БЕРГМУНД',
-     img: 'https://www.ikea.com/ru/ru/images/products/bergmund-stul-barnyy-chernyy-hallarp-bezhevyy__0926462_pe789249_s5.jpg?f=xl',
-     href: 'https://www.ikea.com/ru/ru/p/bergmund-bergmund-stul-barnyy-chernyy-hallarp-bezhevyy-s09388105/',
-     id: 1,
-    },
-];
+async function Content () {
+    let response = await fetch('https://dummyjson.com/products/category/motorcycle');
+    let content = await response.json();
+    return content;
+}
+console.log(Content());
 
 const PersonalCard = () => {
     return (
         <section className={styles.section_card}>
-            <Card title={CONTENT[0].title} img={CONTENT[0].img} href={CONTENT[0].href} />
-            <Card title={CONTENT[0].title} img={CONTENT[0].img} href={CONTENT[0].href} />
-            <Card title={CONTENT[0].title} img={CONTENT[0].img} href={CONTENT[0].href} />
-            <Card title={CONTENT[0].title} img={CONTENT[0].img} href={CONTENT[0].href} />
         </section>
     );
 }
