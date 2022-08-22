b5a321cd4856b2eba46d3c092fe4599335244bde
Alex-Weise <Sannek65@gmail.com>
2022-08-22 19:00:53 +0300

    Изменил визуально и подправил поиск

--- src/components/App/Header/index.tsx
+++ src/components/App/Header/index.tsx
@@ -15,28 +15,24 @@ const Header:FC<THeader> =  ({onClick, setURL, total, setIsHide, isHide}) => {
 
     return (
         <section className={styles.head}>
-            <div className={styles.category} onClick={
-                () => isHide ? setIsHide(false) : setIsHide(true)}>
-               <h3 className={styles.p}> Категории </h3>
+            <div className={styles.category} 
+              onClick={ () => isHide ? setIsHide(false) : setIsHide(true)}>
+                <h3 className={styles.p}> Категории </h3>
             </div>
             <div className={styles.titles} onClick={() => setURL(DEFAULT_URL)} >
                 <h2 className={styles.title}>Собственно товары</h2>
                 <h3 className={styles.total}>Всего {total}</h3>
             </div>
             <div className={styles.serch} >
-              <p className={styles.input}>
-                <input type='text' placeholder='Поиск...' size={25} id="inputSearch"
+                <input className={styles.input} type='text' placeholder='Поиск...' id="inputSearch"
                 onChange={(e) => setValue(e.target.value)} value={value}></input>
-                <span className={!!value ? styles.buttonX : styles.hide}>
-                  <input className={styles.buttonX} type="reset" onClick={ () => setValue('')}
-                  value="X"></input>
-                </span>
-              </p>
-              <p>
-              <input className={styles.button} value="Искать" type="button"
-                 onClick={(e) => {
-                     onClick(value)}}></input>
-              </p>
+                <button className={!!value ? styles.buttonX : styles.hide} type="button" 
+                  onClick={() => {
+                    setValue('');
+                    setURL(DEFAULT_URL)
+                  }}>X</button>
+              <button className={styles.button} type="button"
+                 onClick={() => onClick(value)}>Искать</button>
            </div>
         </section>
     );
